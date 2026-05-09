d4d0da1b7c8e2840dd03bf3090dbeb3488fa7e58
Digital wave <contact2digitalwaveitsolutions@gmail.com>
2026-05-09 11:33:22 +0530

    c

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { useAuthStore } from '../context/authStore';
 
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = process.env.REACT_APP_API_URL;
 
 const api = axios.create({
   baseURL: API_URL,
